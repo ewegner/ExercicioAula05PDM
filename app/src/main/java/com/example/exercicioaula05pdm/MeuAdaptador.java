a2adf3d39a33cc68f3471d77641776ecfb1b9a07
ewegner <wegnereduardo@gmail.com>
2023-10-07 20:18:41 -0300

    Finalizando tabela do campeonato

--- app/src/main/java/com/example/exercicioaula05pdm/MeuAdaptador.java
+++ app/src/main/java/com/example/exercicioaula05pdm/MeuAdaptador.java
@@ -5,6 +5,8 @@ import android.graphics.Color;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
 
 import java.util.List;
 import java.util.Map;
@@ -17,11 +19,30 @@ public class MeuAdaptador extends SimpleAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         View v = super.getView(position, convertView, parent);
-
+        TextView clube = (TextView) v.findViewById(R.id.txtClube);
+        TextView vitorias = (TextView) v.findViewById(R.id.txtVitorias);
+        TextView jogos = (TextView) v.findViewById(R.id.txtJogos);
+        TextView empates = (TextView) v.findViewById(R.id.txtEmpates);
+        TextView pts = (TextView) v.findViewById(R.id.txtPts);
+        TextView derrotas = (TextView) v.findViewById(R.id.txtDerrotas);
         if (position % 2 == 0) {
-            v.setBackgroundColor(Color.parseColor("#7F00FFFF"));
+            v.setBackgroundColor(Color.parseColor("#17980C"));
+
+            clube.setTextColor(Color.parseColor("#FFFFFF"));
+            vitorias.setTextColor(Color.parseColor("#FFFFFF"));
+            jogos.setTextColor(Color.parseColor("#FFFFFF"));
+            empates.setTextColor(Color.parseColor("#FFFFFF"));
+            pts.setTextColor(Color.parseColor("#FFFFFF"));
+            derrotas.setTextColor(Color.parseColor("#FFFFFF"));
         } else {
-            v.setBackgroundColor(Color.parseColor("#7FFF00FF"));
+            v.setBackgroundColor(Color.parseColor("#FFF200"));
+
+            clube.setTextColor(Color.parseColor("#000000"));
+            vitorias.setTextColor(Color.parseColor("#000000"));
+            jogos.setTextColor(Color.parseColor("#000000"));
+            empates.setTextColor(Color.parseColor("#000000"));
+            pts.setTextColor(Color.parseColor("#000000"));
+            derrotas.setTextColor(Color.parseColor("#000000"));
         }
 
         return v;
